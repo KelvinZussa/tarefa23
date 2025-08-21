ebc1fe167e2a25ab72258413f830352fe70855fd
Kelvin Zussa <kelvinzussa@hotmail.com>
2025-08-20 22:42:09 -0300

    tarefa23

--- cypress/e2e/addCartTest.cy.js
+++ cypress/e2e/addCartTest.cy.js
@@ -7,10 +7,11 @@ const { homePage } = require("../support/pages/home.page");
 
 describe('Teste carrinho', () => {
 
-       
+    
+    
     beforeEach(() => {
         cy.login(email, senha)
-    });
+   });
 
     it('Aicionar ao cartinho', () => {
         cart.carrinho()
